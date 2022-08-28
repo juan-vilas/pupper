96a394b3b04db04aa005d1273730e583f8398845
juan-vilas <108366198+juan-vilas@users.noreply.github.com>
2022-08-28 10:28:44 +0200

    puppeteer connect or launch

--- index.ts
+++ index.ts
@@ -4,10 +4,20 @@ import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 puppeteer.use(StealthPlugin());
 
-export default async function getBrowser(): Promise<[Browser, Page]> {
-  let browser = await puppeteer.launch({
-    headless: false,
-  });
+export default async function getBrowser({
+  browserURL = undefined,
+} = {}): Promise<[Browser, Page]> {
+  let browser;
+  let config: any = { defaultViewport: null };
+
+  if (browserURL) {
+    config.browserURL = browserURL;
+    browser = await puppeteer.connect(config);
+  } else {
+    config.headless = false;
+    browser = await puppeteer.launch(config);
+  }
+
   let page = (await browser.pages())[0];
   return [browser, page];
 }
